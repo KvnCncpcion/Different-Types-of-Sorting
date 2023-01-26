fe88597b0e739c05082e808e151c718e17ae9f3d
KvnCncpcion <kvncncpcion@gmail.com>
2023-01-26 22:16:08 +0800

    Slight Modification in the Insertion Sort function

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -4,8 +4,8 @@ print("*****************BSCOE 2-2********************")
 
 def insertion_sort(unique_values):
     for i in range(1, len(unique_values)):
-        j = 1
-        while unique_values[j -1] > unique_values[j] and unique_values > 0:
+        j = i
+        while unique_values[j - 1] > unique_values[j] and j > 0:
             unique_values[j - 1], unique_values[j] = unique_values[j], unique_values[j - 1]
             j -= 1
 

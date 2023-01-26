033b17036c7af78a8c9cceff8ea3a9a6e90d9b93
KvnCncpcion <kvncncpcion@gmail.com>
2023-01-26 22:02:50 +0800

    Defining the function for Insertion Sort

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -2,10 +2,12 @@ print("***************PROGRAMMED BY******************")
 print("**********Kevin Joseph G. Concepcion**********")
 print("*****************BSCOE 2-2********************")
 
-
-
-
-
+def insertion_sort(unique_values):
+    for i in range(1, len(unique_values)):
+        j = 1
+        while unique_values[j -1] > unique_values[j] and unique_values > 0:
+            unique_values[j - 1], unique_values[j] = unique_values[j], unique_values[j - 1]
+            j -= 1
 
 
 unique_values = [57, 26, 28, 88, 55, 78, 90, 79, 27, 35]

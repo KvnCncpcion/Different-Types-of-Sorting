9b69c9c27bc5c70e4f46aa71d2a181e9ff1075a3
KvnCncpcion <kvncncpcion@gmail.com>
2023-01-26 21:47:17 +0800

    Defining the function for Bubble Sort

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -3,8 +3,13 @@ print("**********Kevin Joseph G. Concepcion**********")
 print("*****************BSCOE 2-2********************")
 
 
-
-
+def bubble_sort(unique_values):
+    for i in range(len(unique_values)-1,0,-1):
+        for j in range(i):
+            if unique_values[j] > unique_values[j+1]:
+                temp = unique_values[j]
+                unique_values[j] = unique_values[j+1]
+                unique_values[j+1] = temp
 
 
 unique_values = [57, 26, 28, 88, 55, 78, 90, 79, 27, 35]

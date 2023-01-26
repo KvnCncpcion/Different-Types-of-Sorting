416df5da9374c01913b711f1e71ae82c163d1d25
KvnCncpcion <kvncncpcion@gmail.com>
2023-01-26 21:39:19 +0800

    Adding the instructions on what should the program do and the reference link for the code

--- Selection Sort.py
+++ Selection Sort.py
@@ -2,6 +2,8 @@ print("***************PROGRAMMED BY******************")
 print("**********Kevin Joseph G. Concepcion**********")
 print("*****************BSCOE 2-2********************")
 
+print("Using Selection Sort")
+print("\nThis should be the elements contained in the array: [57, 26, 28, 88, 55, 78, 90, 79, 27, 35]")
 
 def selection_sort(unique_values):
     for i in range(0, len(unique_values) - 1):
@@ -14,4 +16,6 @@ def selection_sort(unique_values):
 
 unique_values = [57, 26, 28, 88, 55, 78, 90, 79, 27, 35]
 selection_sort(unique_values)
-print("This is the result in using the Selection Sort function:",unique_values)
+print("\nThis is the result in using the Selection Sort function:",unique_values)
+
+# Reference for code: https://www.youtube.com/watch?v=ee80YmiaSVQ

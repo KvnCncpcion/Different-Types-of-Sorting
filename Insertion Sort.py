6d71ec22d8ca4dc489720c2a6f85bdab254297b9
KvnCncpcion <kvncncpcion@gmail.com>
2023-01-26 22:22:14 +0800

    Adding the instruction of what the program should do & the reference for the code

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -2,6 +2,9 @@ print("***************PROGRAMMED BY******************")
 print("**********Kevin Joseph G. Concepcion**********")
 print("*****************BSCOE 2-2********************")
 
+print("Using Insertion Sort")
+print("\nThis should be the elements contained in the array: [57, 26, 28, 88, 55, 78, 90, 79, 27, 35]")
+
 def insertion_sort(unique_values):
     for i in range(1, len(unique_values)):
         j = i
@@ -12,4 +15,6 @@ def insertion_sort(unique_values):
 
 unique_values = [57, 26, 28, 88, 55, 78, 90, 79, 27, 35]
 insertion_sort(unique_values)
-print("This is the result in using the Insertion Sort function:",unique_values)
+print("\nThis is the result in using the Insertion Sort function:",unique_values)
+
+# Reference for the code: https://www.youtube.com/watch?v=R_wDA-PmGE4

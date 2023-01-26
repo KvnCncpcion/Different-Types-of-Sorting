696d490741e70e8543687de3b8437ffe8c9e488d
KvnCncpcion <kvncncpcion@gmail.com>
2023-01-26 21:52:30 +0800

    Adding the instruction of what the program should do and the reference for the code

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -2,6 +2,8 @@ print("***************PROGRAMMED BY******************")
 print("**********Kevin Joseph G. Concepcion**********")
 print("*****************BSCOE 2-2********************")
 
+print("Using Bubble Sort")
+print("\nThis should be the elements contained in the array: [57, 26, 28, 88, 55, 78, 90, 79, 27, 35]")
 
 def bubble_sort(unique_values):
     for i in range(len(unique_values)-1,0,-1):
@@ -14,4 +16,6 @@ def bubble_sort(unique_values):
 
 unique_values = [57, 26, 28, 88, 55, 78, 90, 79, 27, 35]
 bubble_sort(unique_values)
-print("This is the result in using the Bubble Sort function:",unique_values)
+print("\nThis is the result in using the Bubble Sort function:",unique_values)
+
+# Reference for the code: https://www.youtube.com/watch?v=Vca808JTbI8

570389e0831f7ce5aad0c59ad96b87fafe0c988a
KvnCncpcion <kvncncpcion@gmail.com>
2023-01-26 21:33:55 +0800

    Define the function for Selection Sort

--- Selection Sort.py
+++ Selection Sort.py
@@ -3,5 +3,13 @@ print("**********Kevin Joseph G. Concepcion**********")
 print("*****************BSCOE 2-2********************")
 
 
+def selection_sort(unique_values):
+    for i in range(0, len(unique_values) - 1):
+        cur_min_index = i
+        for j in range(i + 1, len(unique_values)):
+            if unique_values[j] < unique_values[cur_min_index]:
+                cur_min_index = j
+
+        unique_values[i], unique_values[cur_min_index] = unique_values[cur_min_index], unique_values[i]
 
 unique_values = [57, 26, 28, 88, 55, 78, 90, 79, 27, 35]
